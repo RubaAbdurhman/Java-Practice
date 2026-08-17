3406f527acf3a2fb497b18715769fdbe25b9b54d
Ruba <rubaalzhrani23@gmail.com>
2026-08-17 15:08:01 +0300

    Add files via upload
     In this code I used a Scanner to the user  input  the Radius.

---
+++ Calculating the Area of ​​a Circle using Scanner (1).java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main (String [] args )
+    {
+        Scanner input= new Scanner(System.in) ;
+        
+        System . out . println (" Enter Ferst Radius ");
+        double theRedius = input . nextDouble ();
+        
+        double area = theRedius * theRedius *3.14159;
+        System . out . println (" the area is " + area );
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
