bb1496f18b504ee8178e828681d3475c455d3b07
Ruba <rubaalzhrani23@gmail.com>
2026-08-17 14:52:32 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    
+    public static void main (String [] args){
+        
+        double  redios;
+        double  area;
+        
+        redios = 20; 
+        
+        area=redios * redios * 3.14159;
+        
+        System . out .println ( "The area for the circal of redios"+ redios + redios +"is"+area );
+    
+    }
+}
