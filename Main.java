aa90b0ffa77074a2abfab189c01d16c5f0ca1766
Ruba <rubaalzhrani23@gmail.com>
2026-08-17 15:16:13 +0300

    Add files via upload
    Calculating  Square Area .

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    /* main method */
+    public static void main (String[] args  ){
+        double base =5;
+        double height=5;
+        double area = (base*height)/2; 
+    
+        base = 5 ;
+        height=5;
+        area=(base*height)/2 ;
+        // Display results 
+System.out.println("The area for the triangle (" + base + "*" + height + ")/2 is " + area);
+    }
+}
