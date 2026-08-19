3388253230b8b8a17665c5ab4f95f2580b82420c
Ruba <rubaalzhrani23@gmail.com>
2026-08-19 19:32:04 +0300

    Add files via upload
    ​A Java program that initializes an integer array, iterates through its elements using a standard for loop, prints each element, and calculates the total sum of the array elements.

---
+++ Iterating and Calculating Array Sum in Java.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+//public class Main//
+//{
+	//public static void main(String[] args) {
+		//System.out.println("Hello World");
+//	}
+//}//
+public class Main{
+public static void main(String[]args){
+ int[]number={1,2,3,4,5};
+ int sum=0;
+ 
+ for(int i =0;i<number.length;i++){
+
+System.out.println( number[i]);
+ sum +=number[i];}
+System.out.println(sum +"=numbers");
+
+}}
