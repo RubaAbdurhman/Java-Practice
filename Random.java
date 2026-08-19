c812ff7c28dcd1abdb0de7b6a13dd991eaa670fd
Ruba <rubaalzhrani23@gmail.com>
2026-08-19 19:28:06 +0300

    Add files via upload
    A Java example demonstrating how to generate a random integer within a specific range using Math.random() combined with explicit type casting (int).

---
+++ Random.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	  int  a=3;
+	  int  b=4;
+	     int x=a+(int)(Math.random()*(a+b));
+		System.out.println(x);
+	}
+}
