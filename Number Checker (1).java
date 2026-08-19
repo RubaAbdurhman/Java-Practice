0cffa947e2cfac153f74a46609e06163fe6c5e47
Ruba <rubaalzhrani23@gmail.com>
2026-08-19 04:01:24 +0300

    Add files via upload
    This program checks whether a number is greater than 10 and determines whether it is even or odd.

---
+++ Number Checker (1).java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+    public  static void main(String[]args){
+      Scanner input = new Scanner (System.in);
+      System.out.print("Enter a number:");
+      double number=input.nextDouble();
+      
+        if (number>10){
+            System.out. println ("the number grater than 10");
+            
+        }else {
+            System. out .println("the number not grater then 10");
+        }
+  
+          if(number%2==0){
+          System. out. println ("Even");
+          }else{
+              System. out. println("ood ");
+          } 
+         
+ } 
+    } 
